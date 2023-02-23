41395005d6b5ebcc0e905bfc872df48bad585888
Cristian Campos Agüero <ccampos04@gmail.com>
2023-02-22 23:34:14 -0600

    Update testing.py

--- testing.py
+++ testing.py
@@ -1,34 +1,29 @@
-import Portafolio1;
+import Practica;
 import pytest;
 
-def test_divisores_1():
-    assert Portafolio1.divisores(24) == [24, 12, 8, 6, 4, 3, 2, 1]
-      
-###########################################################################
-
-def test_potencia_1():
-    assert Portafolio1.potencia(5, 2) == 25
+def test_1():
+    assert Practica.sumatoria(5) == 15
     
-###########################################################################    
-
-def test_division_1():
-    assert Portafolio1.division(25, 5) == 5
+def test_2():
+    assert Practica.factorial(0) == 1
     
-def test_division_2():
-    assert isinstance(str(Portafolio1.division(25, 0)), str) == isinstance('Error: División entre cero', str)
+def test_3():
+    assert Practica.factorial(3) == 6
     
-###########################################################################       
-
-def test_indiceNumero_1():
-    assert Portafolio1.indiceNumero(1235, 3) == 3
+def test_4():
+    assert Practica.sumatoriaDescendente(5) == 15
     
-def test_indiceNumero_2():
-    assert isinstance(str(Portafolio1.indiceNumero(1335, 8)), str) == isinstance('Error: Indice fuera del rango del número', str)
+def test_5():
+    assert Practica.sumatoriaParcial(3, 6) == 18
     
-###########################################################################    
-
-def test_cortarNumero_1():
-    assert Portafolio1.cortarNumero(1235, 2, 3) == 23
-
-def test_cortarNumero_2():
-    assert isinstance(Portafolio1.cortarNumero(1335, 8, 2), str) == isinstance('Error: Indices fuera del rango del número', str)  
+def test_6():
+    assert Practica.contarDigitos(306) == 3
+    
+def test_7():
+    assert Practica.contarDigitos(0) == 1
+    
+def test_8():
+    assert Practica.contarDigitosImpares(305) == 2
+    
+def test_9():
+    assert Practica.contarDigitosImpares(0) == 0
